af63dd7ee7bd3c89986cd1fdc44f3b37c6b79f1d
ASHIRVAD KUMAR <114767026+ASHIRVAD363@users.noreply.github.com>
2023-09-23 21:41:46 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+        strings: [
+            "<span> Hii, I am Ashirvad </span> web developer , learner andI am a front-end website developer and mostly I am passionate on web coding.<br><br>  B.Tech student specializing in Computer Science Engineering. With a knack for front-end development, also excel in creating seamless and visually appealing user interfaces. I am proficiency in HTML, CSS, and JavaScript, coupled with strong teamwork skills, positions them as a valuable asset in any collaborative project."
+        ],
+        typeSpeed: 12,
+        contentType: 'html'
+    });
+}, 100);
